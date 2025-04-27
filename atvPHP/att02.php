1f53c1753ffa3de2aebb94bd5b433fe341ccd50e
AIsaac10 <168681995+AIsaac10@users.noreply.github.com>
2025-04-27 16:21:57 -0300

    Update att02.php

--- atvPHP/att02.php
+++ atvPHP/att02.php
@@ -2,13 +2,13 @@
 números lidos. -->
 
 <?php 
-$num1 = 6;
-$num2= 4;
-echo $num1 + $num2;
-echo '<br>';
-echo $num1 - $num2;
-echo '<br>';
-echo $num1 * $num2;
-echo '<br>';
-echo $num1 / $num2;
+  $num1 = 6;
+  $num2= 4;
+  echo $num1 + $num2;
+  echo '<br>';
+  echo $num1 - $num2;
+  echo '<br>';
+  echo $num1 * $num2;
+  echo '<br>';
+  echo $num1 / $num2;
 ?>
